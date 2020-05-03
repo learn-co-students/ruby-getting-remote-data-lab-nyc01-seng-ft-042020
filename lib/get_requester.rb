96f61bdf59dd9b28326aedfb72f922c572a65f1f
rwilt <rosie.wilt@gmail.com>
2020-05-03 12:30:48 -0400

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,32 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+ attr_accessor :url #do i need this? why not? 
+    def initialize(url)
+        @url = url
+    end
+
+    def get_response_body
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+  
+
+    def parse_json
+        parsed = JSON.parse(self.get_response_body)
+        # parsed.collect do |element|
+        #     element
+        #  end 
+        #we dont need ot iterate over anything 
+        #because we are just asking for a general 
+        #parsed JSON object with all data. 
+        parsed
+    end
+
+ 
+
+end
